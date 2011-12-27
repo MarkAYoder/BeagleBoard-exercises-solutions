58fa8eb82e3ed84bde2c8f5d4d22279275ad5d3c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2011-12-27 19:37:01 +0000

    Cleaned up formatting.

--- VideoThru/lab08b_audio_video/thread.c
+++ VideoThru/lab08b_audio_video/thread.c
@@ -43,63 +43,50 @@ int launch_pthread( pthread_t *hThread_byref,
     int status = thread_SUCCESS;
 
     /* Initialize thread attributes structures */
-    if( pthread_attr_init( &threadAttrs ) )
-    {
+    if( pthread_attr_init( &threadAttrs ) ) {
         ERR( "threadAttrs initialization failed\n" );
         status = EXIT_FAILURE;
         goto cleanup;
     }
 
-
     /* This library defaults to inherited scheduling characteristics!   */
     /* If you don't set the inheritance, no changes will take place!    */
 
-    if( pthread_attr_setinheritsched( &threadAttrs, PTHREAD_EXPLICIT_SCHED ) )
-    {
+    if( pthread_attr_setinheritsched( &threadAttrs, PTHREAD_EXPLICIT_SCHED ) ) {
         ERR( "audioThreadAttrs set scheduler inheritance failed\n" );
         status = EXIT_FAILURE;
         goto cleanup;
     }
 
-
     /* Setthread scheduling policy to real-time or time-slice           */
     /* SCHED_RR available only to threads running as superuser          */
 
-    if(type == REALTIME)
-    {
-        if( pthread_attr_setschedpolicy( &threadAttrs, SCHED_RR ) )
-        {
+    if(type == REALTIME) {
+        if( pthread_attr_setschedpolicy( &threadAttrs, SCHED_RR ) ) {
             ERR( "pthread_attr_setschedpolicy failed\n" );
             status = EXIT_FAILURE;
             goto cleanup;
         }
-    } 
-    else 
-    {
-        if( pthread_attr_setschedpolicy( &threadAttrs, SCHED_OTHER ) )
-        {
+    } else {
+        if( pthread_attr_setschedpolicy( &threadAttrs, SCHED_OTHER ) ) {
             ERR( "pthread_attr_setschedpolicy failed\n" );
             status = EXIT_FAILURE;
             goto cleanup;
         }
     }
 
-
     /* Set thread priority */
     threadParams.sched_priority = priority;
 
-    if( pthread_attr_setschedparam( &threadAttrs, &threadParams ) )
-    {
+    if( pthread_attr_setschedparam( &threadAttrs, &threadParams ) ) {
         ERR( "pthread_attr_setschedparam failed\n" );
         status = EXIT_FAILURE;
         goto cleanup;
     }
  
-
     /*  Create the thread  */
 
-    if ( pthread_create(hThread_byref, &threadAttrs, thread_fxn, env ) ) 
-    {
+    if ( pthread_create(hThread_byref, &threadAttrs, thread_fxn, env ) ) {
         ERR( "Failed to create thread\n" );
         status = EXIT_FAILURE;
         goto cleanup;
